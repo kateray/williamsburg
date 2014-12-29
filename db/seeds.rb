4cfa14806fdc025f786d5a9b3aba7f43cd928ee3
Kate Ray <email@kateray.net>
2014-12-29 16:04:40 -0500

    Bad data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,8 +12,8 @@ City.create(name: 'melbourne')
 City.create(name: 'san-francisco')
 
 
-AdminPin.create(city_id: 1, neighborhood: 'williamsburg', latitude: 40.7133, longitude: 73.9533)
+AdminPin.create(city_id: 1, neighborhood: 'williamsburg', latitude: 40.7133, longitude: -73.9533)
 AdminPin.create(city_id: 2, neighborhood: 'kreuzberg', latitude: 52.4875, longitude: 13.3833)
 AdminPin.create(city_id: 3, neighborhood: 'shoreditch', latitude: 51.5260, longitude: 0.0780)
 AdminPin.create(city_id: 4, neighborhood: 'fitzroy', latitude: -37.8011, longitude: 144.9789)
-AdminPin.create(city_id: 5, neighborhood: 'mission', latitude: 37.7600, longitude: 122.4200)
+AdminPin.create(city_id: 5, neighborhood: 'mission', latitude: 37.7600, longitude: -122.4200)
