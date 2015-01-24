b8af1d3d308deaa945675abf27ba5d5d21916ddb
Kate Ray <email@kateray.net>
2015-01-24 16:50:05 -0500

    Updated add pins

--- app/controllers/user_pins_controller.rb
+++ app/controllers/user_pins_controller.rb
@@ -2,27 +2,42 @@ class UserPinsController < ApplicationController
 
 	def add
 		#Authentication
-		if params[:aid] != ENV["APP_ID"]
+		if !params[:aid] || params[:aid] != '1413358A-9CE6-4E27-A142-4A2AE4AF20A2'
+		# if params[:aid] != ENV["APP_ID"]
 			render :json => {error: "Unauthenticated request"}, :status => :unprocessable_entity
 			return false
 		end
 
 		device_id = params[:did]
-		name = params[:c].downcase
-		@city = City.find_or_create_by(name: name)
+		lat = params[:lt].to_f
+		long = params[:lg].to_f
 
-		@pin = @city.user_pins.find_by_token(device_id) || UserPin.create(token: device_id)
-		@pin.latitude = params[:lt].to_f
-		@pin.longitude = params[:lg].to_f
-		@pin.neighborhood = params[:n].downcase
-		@pin.city_id = @city.id
+		@city = City.near([lat, long], 50).first
 
-		if @pin.save
-			@city.calculate_location
-			head :ok
+		if @city == nil
+			result = Geocoder.search(lat.to_s + ',' + long.to_s).first
+			if result && result.city
+				@city = City.create(name: result.city, latitude: lat, longitude: long)
+				@pin = UserPin.create(token: device_id, latitude: lat, longitude: long, city_id: @city.id)
+				head :ok
+			else
+				render json: {error: "Unable to save"}, :status => :unprocessable_entity
+				return false
+			end
 		else
-			render :json => {error: "Unable to save"}, :status => :unprocessable_entity
-			return false
+			@pin = @city.user_pins.find_by_token(device_id) || UserPin.create(token: device_id)
+			@pin.latitude = lat
+			@pin.longitude = long
+			@pin.city_id = @city.id
+
+			if @pin.save
+				@city.calculate_location
+				head :ok
+			else
+				render json: {error: "Unable to save"}, :status => :unprocessable_entity
+				return false
+			end
+
 		end
 	end
 
