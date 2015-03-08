57d5266626d3a38e965be13f535f4c03f3e31866
Kate Ray <email@kateray.net>
2015-03-08 13:58:50 -0400

    Fix Masalana data

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,7 +43,7 @@ AdminPin.create(city_id: 6, neighborhood: 'North Park', latitude: 32.7408, longi
 AdminPin.create(city_id: 7, neighborhood: 'Somerville', latitude: 42.3875, longitude: -71.1000)
 AdminPin.create(city_id: 8, neighborhood: 'Wicker Park', latitude: 41.9075, longitude: -87.6769)
 AdminPin.create(city_id: 9, neighborhood: 'Silver Lake', latitude: 34.0944, longitude: -118.2675)
-AdminPin.create(city_id: 10, neighborhood: 'Malasaña', latitude: 40.4253, longitude: 3.7083)
+AdminPin.create(city_id: 10, neighborhood: 'Malasaña', latitude: 40.4253, longitude: -3.7083)
 AdminPin.create(city_id: 11, neighborhood: 'The Grove', latitude: 38.6270, longitude: -90.2569)
 AdminPin.create(city_id: 12, neighborhood: 'U Street', latitude: 38.9170, longitude: -77.0296)
 AdminPin.create(city_id: 13, neighborhood: 'South Congress', latitude: 30.2500, longitude: -97.7500)
