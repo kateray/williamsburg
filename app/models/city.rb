d9948eeacb014d684b002c444ade341adacb3608
Kate Ray <email@kateray.net>
2016-09-24 15:19:49 -0400

    Discard admin pins

--- app/models/city.rb
+++ app/models/city.rb
@@ -17,23 +17,9 @@ class City < ActiveRecord::Base
 		new_long = nil
 		new_neighborhood = nil
 
-		if self.admin_pin
-			new_neighborhood = self.admin_pin.neighborhood
-			#City has both an admin pin and some user pins, so give a weighted average
-			if self.user_pins.any?
-				new_lat = (self.average_lat + self.admin_pin.latitude) / 2
-				new_long = (self.average_long + self.admin_pin.longitude) / 2
-			#City only has an admin pin, so just use that
-			else
-				new_lat = self.admin_pin.latitude
-				new_long = self.admin_pin.longitude
-			end
-		#City only has user pins, so give an average
-		elsif self.user_pins.any?
-			new_neighborhood = self.user_pins.first.neighborhood
-			new_lat = self.average_lat
-			new_long = self.average_long
-		end
+		new_neighborhood = self.user_pins.first.neighborhood
+		new_lat = self.average_lat
+		new_long = self.average_long
 
 		if new_lat && new_long
 			if (new_lat != self.latitude) || (new_long != self.longitude)
