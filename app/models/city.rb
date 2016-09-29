5d9ecd8a0f46ae23a6985d2986b0775c1d560a1b
Kate Ray <email@kateray.net>
2016-09-29 11:03:56 -0400

    New update cities

--- app/models/city.rb
+++ app/models/city.rb
@@ -40,30 +40,18 @@ class City < ActiveRecord::Base
     self.user_pins.where(used_neighborhood: self.get_pin_mode).count
   end
 
-  def get_center
+  def get_center(place)
     search_array = []
-    self.user_pins.where(used_neighborhood: self.get_pin_mode).each do |p|
+    self.user_pins.where(used_neighborhood: place).each do |p|
       search_array.push([p.latitude, p.longitude])
     end
     Geocoder::Calculations.geographic_center(search_array)
   end
 
 	def calculate_location
-		new_lat = nil
-		new_long = nil
-		new_neighborhood = nil
-
-		new_neighborhood = self.user_pins.first.neighborhood
-		new_lat = self.average_lat
-		new_long = self.average_long
-
-		if (new_lat != self.latitude) || (new_long != self.longitude)
-			self.latitude = new_lat
-			self.longitude = new_long
-			self.neighborhood = new_neighborhood
-			puts "Saving #{self.name}"
-			self.save
-		end
+    neighborhood = self.get_pin_mode
+    center = self.get_center(neighborhood)
+    self.update_attributes(neighborhood: neighborhood, latitude: center[0], longitude: center[1])
 	end
 
 	def as_json(options={})
